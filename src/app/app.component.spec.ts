3adc16bc93fdcd27e54ee55c4fc7ec3919ed6816
Praveen Katha <praveenkatha16@gmail.com>
2021-06-25 21:19:48 +0530

    changes..

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AlertService } from './alert.service';
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let service: jasmine.SpyObj<DataService>;
+  let alertService: jasmine.SpyObj<AlertService>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      declarations: [AppComponent],
+    })
+      .overrideProvider(DataService, {
+        useValue: jasmine.createSpyObj<DataService>('DataService', ['init', 'getFilteredData']),
+      })
+      .overrideProvider(AlertService, {
+        useValue: jasmine.createSpyObj<AlertService>('AlertService', [
+          'showAlert',
+        ]),
+      })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+    service = TestBed.inject(DataService) as jasmine.SpyObj<DataService>;
+    alertService = TestBed.inject(AlertService) as jasmine.SpyObj<AlertService>;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-sample-v10-pre'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-sample-v10-pre');
+  it('init and showAlert should be called', () => {
+    app.ngOnInit();
+    expect(service.init).toHaveBeenCalled();
+    expect(alertService.showAlert).toHaveBeenCalled();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-sample-v10-pre app is running!');
+  it('app data should be initialized', () => {
+    const mockRes = [{ id: 1, name: 'Hello' }];
+    service.getFilteredData.and.returnValue(mockRes);
+    app.ngOnInit();
+    expect(app.data).toEqual(mockRes);
   });
 });
